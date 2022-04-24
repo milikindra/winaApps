e787c88f763704c3ba190da0e1f30cf9c33fef70
Indra Purnomo <indra@viktori-automation.com>
2022-04-24 13:46:58 +0000

    constants.php edited online with Bitbucket

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_url' => "http://wina.viktori-automation.com:81/service/public",
+    'api_url' => "http://localhost/service/public",
     'version' => "1",
     'tokeen' => "62df5535ad5f5dd696cda9de473f6a3191a830b4549fa6273f6aea09ef94bfa307660d25aea5b84beba11c6e5991403a"
 ];
