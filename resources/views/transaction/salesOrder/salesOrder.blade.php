9ee78c719da065f8fa58e31041dba4374d82643b
vpa_indra <indra@viktori-automation.com>
2022-05-20 14:32:04 +0700

    fix so qty

--- resources/views/transaction/salesOrder/salesOrder.blade.php
+++ resources/views/transaction/salesOrder/salesOrder.blade.php
@@ -40,7 +40,7 @@
                             </div>
                             <div class="col-12">
                                 <label>Kategori</label>
-                                <select class="form-control selects2" id="kategoriFilter">
+                                <select class="form-control selects2" id="kategoriFilter" width="100%">
                                     <option value="outstanding" selected>Outstanding</option>
                                     <option value="lunas">Lunas</option>
                                     <option value="all">All</option>
@@ -63,25 +63,26 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <form method="POST" action="{{ route('salesOrder') }}">
-                            @csrf
-                            <div class="row">
-                                <div class="col-12">
-                                    <br />
-                                    <label>Tanggal Awal</label>
-                                    <input type="date" class="form-control" name="sdate" value="{{date('Y-m-d', strtotime('-1 year'))}}" required>
-                                </div>
-                                <div class="col-12">
-                                    <br />
-                                    <label>Tanggal Akhir</label>
-                                    <input type="date" class="form-control" name="edate" value="{{date('Y-m-d')}}" required>
-                                </div>
-                                <div class="col-12">
-                                    <br />
-                                    <button type="submit" class="btn btn-flat btn-info float-right">Prosess</button>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="fdate"></input>
+                                    <label>Filter Tanggal</label>
                                 </div>
                             </div>
-                        </form>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <br />
+                                <label>Tanggal Awal</label>
+                                <input type="date" class="form-control" name="sdate" id="sdate" value="{{date('Y-m-d', strtotime('-1 year'))}}" required>
+                            </div>
+                            <div class="col-12">
+                                <br />
+                                <label>Tanggal Akhir</label>
+                                <input type="date" class="form-control" name="edate" id="edate" value="{{date('Y-m-d')}}" required>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -101,7 +102,7 @@
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id="datatables" class="table table-bordered table-striped table-hover">
+                        <table id="datatables" class="table table-bordered table-striped table-hover" width="100%">
                             <thead id="dtheader">
 
                             </thead>
@@ -251,140 +252,6 @@
     </div>
 </div>
 
-<div class="modal fade" id="editInventory" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content">
-            <form action="{{ route('inventoryUpdate') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="modal-header bg-info">
-                    <h5 class="modal-title">Edit Inventory</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Kode</label>
-                            <div class="col-sm-4">
-                                <input type="text" class="form-control" id="kode" name="kode" placeholder="Masukkan Kode" required>
-                            </div>
-                            <div class="col-sm-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="aktifEdit" name="aktif" checked></input>
-                                <label>AKTIF</label>
-                            </div>
-                            <div class="col-sm-3">
-                                <input type="checkbox" class="form-check-input" id="konsinyansiEdit" name="konsinyansi"></input>
-                                <label>KONSINYANSI</label>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Nama Barang</label>
-                            <div class="col-sm-10">
-                                <textarea class="form-control" id="nama_barang" name="nama_barang" rows="2" placeholder="Masukkan Nama Barang"></textarea>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Satuan</label>
-                            <div class="col-sm-4">
-                                <input type="text" class="form-control" id="satuan" name="satuan" maxlength="5" placeholder="Masukkan satuan">
-                            </div>
-                            <label class="col-sm-2 col-form-label">Stok Minimal</label>
-                            <div class="col-sm-4">
-                                <input type="number" class="form-control" id="stok_minimal" name="stok_minimal" placeholder="Masukkan Stok Minimal" value="0" min="0">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Kategori</label>
-                            <div class="col-sm-4">
-                                <select class="form-control selects2" id="kategoriEdit" name="kategori">
-                                    <option value="">Pilih Kategori</option>
-
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Subkategori</label>
-                            <div class="col-sm-4">
-                                <select class="form-control selects2" id="subkategoriEdit" name="subkategori">
-                                    <option value="">Pilih Subkategori</option>
-
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Merk</label>
-                            <div class="col-sm-4">
-                                <select class="form-control selects2" id="merkEdit" name="merk">
-                                    <option value="">Pilih Merk</option>
-
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Harga Jual</label>
-                            <div class="col-sm-4">
-                                <input type="text" class="form-control numaja" id="harga_jual" name="harga_jual" placeholder="Masukkan Harga Jual">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Keterangan</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="keterangan" name="keterangan" placeholder="keterangan">
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="form-group row">
-                            <div class="col-sm-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="isMinusEdit" name="isMinus"></input>
-                                <label>NON INVENTORY</label>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Sales Account</label>
-                            <div class="col-sm-4">
-                                <select class="form-control selects2" id="salesAccEdit" name="salesAcc">
-                                    <option></option>
-
-                                </select>
-                            </div>
-                            <div class="col-sm-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="PphPs23Edit" name="PphPs23"></input>
-                                <label>PPh Ps 23</label>
-                            </div>
-                            <div class="col-sm-3">
-                                <input type="checkbox" class="form-check-input" id="PPhPs21Edit" name="PPhPs21"></input>
-                                <label>PPh Ps 21</label>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Purchase Account</label>
-                            <div class="col-sm-4">
-                                <select class="form-control selects2" id="purchaseAccEdit" name="purchaseAcc">
-                                    <option></option>
-
-                                </select>
-                            </div>
-                            <div class="col-sm-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="PPhPs4Ayat2Edit" name="PPhPs4Ayat2"></input>
-                                <label>PPh Ps 4 ayat 2</label>
-                            </div>
-                            <div class="col-sm-3">
-                                <input type="checkbox" class="form-check-input" id="PPhPs21OPEdit" name="PPhPs21OP"></input>
-                                <label>PPh Ps 21 OP</label>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class=" modal-footer justify-content-between">
-                    <button type="button" class="btn btn-flat btn-default" style="text-align:left;" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-flat btn-info">Save</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 
 <!-- end modal -->
 @endsection
