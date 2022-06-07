7296282e9bb198a452a7c07791dcf87e1711b0b9
vpa_indra <indra@viktori-automation.com>
2022-06-07 14:29:47 +0700

    fix head konsi

--- resources/views/report/stock/excel/reportPosisiStockTmp.blade.php
+++ resources/views/report/stock/excel/reportPosisiStockTmp.blade.php
@@ -110,25 +110,14 @@ echo "Some Text"; //no ending ; here
         <table class="tableBorder fixed-bottom" style="margin-top: 5px; clear: both; top: 80px; border: 0.50px solid gray;">
             <thead>
                 <tr style="border-collapse: collapse; border: 2px solid #ddd;">
-<<<<<<< .mine
-                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 0.50px solid gray;" width="10%">Kode</th>
-                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 0.50px solid gray;" width="10%">Nama Barang</th>
-                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 0.50px solid gray;" width="10%">Satuan</th>
-                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 0.50px solid gray;" width="10%">Qty</th>
-                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 0.50px solid gray;" width="10%">Total</th>
-                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 0.50px solid gray;" width="10%">Harga Rata-rata</th>
-                </tr>
-
-=======
                     <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Kode</th>
                     <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Nama Barang</th>
                     <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Satuan</th>
                     <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Qty</th>
                     <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Total</th>
-		    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Harga Rata-rata</th>
-		    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Status Konsinyasi</th>
-		</tr>
->>>>>>> .theirs
+                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Harga Rata-rata</th>
+                    <th style="font-family: helvetica,sans-serif;font-size: 10px; font-weight: 700;border: 1px solid black;" width="10%">Status Konsinyasi</th>
+                </tr>
             </thead>
             <tbody>
                 <?php
@@ -145,15 +134,15 @@ echo "Some Text"; //no ending ; here
                         <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500;border: 0.50px solid gray; text-align:right;">{{number_format($posisi->qty,2) }}</td>
                         <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500;border: 0.50px solid gray; text-align:right;">{{number_format($posisi->jml_pok,2) }}</td>
                         <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500;border: 0.50px solid gray; text-align:right;">{{number_format($posisi->rata,2) }}</td>
-			<td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500;border: 1px solid black; text-align:center">{{$posisi->isKonsi }}</td>
-		    </tr>
+                        <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500;border: 1px solid black; text-align:center">{{$posisi->isKonsi }}</td>
+                    </tr>
                 <?php } ?>
                 <tr>
                     <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500; border: 0.50px solid gray; text-align:right;" colspan="4"><b>Total</b></td>
                     <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500; border: 0.50px solid gray; text-align:right;">{{number_format($total,2) }}</td>
                     <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500; border: 0.50px solid gray; text-align:right;">{{number_format($total_rata,2) }}</td>
-		    <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500; border: 1px solid black; text-align:right;"></td>
-		</tr>
+                    <td style="font-family: helvetica,sans-serif;font-size: 9px; font-weight: 500; border: 1px solid black; text-align:right;"></td>
+                </tr>
             </tbody>
         </table>
 
