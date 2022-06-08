bd4a6e86d8964634639d6ed17f48952823cf0cb1
Indra <indra@smtp.viktori-automation.com>
2022-06-08 15:45:08 +0700

    Small bugfixed on View Print - Report Helper Transmittal - Change Input "email" to <label>

--- resources/views/report/helper/print/reportTransmitalReceipt.blade.php
+++ resources/views/report/helper/print/reportTransmitalReceipt.blade.php
@@ -185,8 +185,8 @@
             <div class="form-group row justify-content-md-center">
                 <div class="col-sm-8 form-inline">
                     <input class="custom-control-input custom-control-input-dark custom-control-input-outline" type="checkbox" id="trEmailCb">
-                    <label for="trEmailCb" class="custom-control-label">Email to : </label>
-                    <input type="email" class="form-control form-control-border" name="trEmailCb" style="width: 40%;" id="trEmailCb" value="fiance-ar@viktori-automation.com">
+		    <label for="trEmailCb" class="custom-control-label">Email to  </label>
+		    <label>: {{$val['trEmailCb']}}</label>
                 </div>
             </div>
             <div class="form-group row">
